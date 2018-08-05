f13f0ca8c3d7f1caf0317e24f09b2884647590c6
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 23:25:03 +0530

    Update SavingAccountTest2.java

--- SavingAccountTest2.java
+++ SavingAccountTest2.java
@@ -8,13 +8,14 @@ import java.util.TreeSet;
 
 public class SavingAccountTest2 {
 	public static void main(String args[]) {
+		
 		SavingAccount savingaccount1 = new SavingAccount(300000, 990, "For", true);
 		SavingAccount savingaccount2 = new SavingAccount(30000, 204, "Ben", false);
 		SavingAccount savingaccount3 = new SavingAccount(200000, 18, "Chrevo", true);
 		SavingAccount savingaccount4 = new SavingAccount(500000, 996, "Ast", true);
 		SavingAccount savingaccount5 = new SavingAccount(60000, 523, "Ki", false);
 		
-		
+		//Creating Set for Saving Account Members
 		Set<SavingAccount> savingacc = new TreeSet<SavingAccount>();
 		savingacc.add(savingaccount1);
 		savingacc.add(savingaccount2);
@@ -22,6 +23,7 @@ public class SavingAccountTest2 {
 		savingacc.add(savingaccount4);
 		savingacc.add(savingaccount5);
 		
+		// Printing the values using lambda expression
 		savingacc.stream().forEach(System.out::println);
 		
 	
