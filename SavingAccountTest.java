04c1152133b7579ee6dd5199784379b856eadf8a
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-05 23:24:02 +0530

    Update SavingAccountTest.java

--- SavingAccountTest.java
+++ SavingAccountTest.java
@@ -5,13 +5,14 @@ import java.util.List;
 public class SavingAccountTest {
 	
 	public static void main(String args[]) {
+		
 		SavingAccount savingaccount1 = new SavingAccount(3000000, 1990, "Ford", true);
 		SavingAccount savingaccount2 = new SavingAccount(3000000, 1204, "Benz", false);
 		SavingAccount savingaccount3 = new SavingAccount(3000000, 2018, "Chrevolet", true);
 		SavingAccount savingaccount4 = new SavingAccount(3000000, 1996, "Aston", true);
 		SavingAccount savingaccount5 = new SavingAccount(3000000, 4523, "Kia", false);
 		
-		
+		//Creating list of Saving Account members
 		List<SavingAccount> savingacc = new ArrayList<SavingAccount>();
 		savingacc.add(savingaccount1);
 		savingacc.add(savingaccount2);
@@ -19,6 +20,7 @@ public class SavingAccountTest {
 		savingacc.add(savingaccount4);
 		savingacc.add(savingaccount5);
 		
+		// Printing the values
 		for (SavingAccount savingaccount : savingacc) {
 			System.out.println(savingaccount);
 		}
@@ -31,6 +33,7 @@ public class SavingAccountTest {
 		savingaccount4.deposit(456);
 		savingaccount5.deposit(186);
 		
+		// Printing the values using lambda expression
 		savingacc.stream().forEach(System.out::println);
 		
 		System.out.println("######################################################################################");
@@ -41,6 +44,7 @@ public class SavingAccountTest {
 		savingaccount4.withdraw(166);
 		savingaccount5.withdraw(126);
 		
+		// Printing the values using lambda expression
 		savingacc.stream().forEach(System.out::println);
 		
 
